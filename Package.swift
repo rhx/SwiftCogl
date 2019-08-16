dfcd3858db9c59217cd4ee2d5ee225fc5739dc29
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-17 07:57:17 +1000

    Integrate CCogl as a systemLibrary

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
@@ -8,11 +8,15 @@ let package = Package(
         .library(name: "Cogl", targets: ["Cogl"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/CCogl.git", .branch("master")),
         .package(url: "https://github.com/rhx/SwiftGObject.git", .branch("master")),
     ],
     targets: [
-        .target(name: "Cogl", dependencies: ["GLibObject"]),
+	.systemLibrary(name: "CCogl", pkgConfig: "cogl-gl-1.0 cogl-path-1.0 ccairo glib-2.0 gio-unix-2.0",
+	    providers: [
+		.brew(["cogl", "cairo", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libcogl-gles2-dev", "libcogl-path-dev", "libcogl-dev", "libcairo2-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	    ]),
+        .target(name: "Cogl", dependencies: ["CCogl", "GLibObject"]),
         .testTarget(name: "CoglTests", dependencies: ["Cogl"]),
     ]
 )
