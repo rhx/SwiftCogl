c38f3a84d7e5857a26572b5956a3a121716bc3f1
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-16 08:08:13 +1000

    Rename GdkPixbuf namespace to GdkPixBuf to avoid clash with C type name

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
                 "CCogl",
                 "CCoglPath",
                 .product(name: "gir2swift", package: "gir2swift"),
-                .product(name: "GdkPixbuf", package: "SwiftGdkPixbuf"),
+                .product(name: "GdkPixBuf", package: "SwiftGdkPixbuf"),
             ],
             swiftSettings: [
                 .unsafeFlags(["-suppress-warnings"], .when(configuration: .release)),
